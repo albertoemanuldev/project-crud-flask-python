fb9802106ade1c397ebb178c471add8191fd528b
albertoemanuldev <albertoemanuel.dev@gmail.com>
2026-04-22 21:03:28 -0300

    Mudanças na interface do dashboard para um tema mais escuro, com cores mais suaves e modernas. O objetivo é criar um ambiente visualmente agradável e confortável para os usuários, reduzindo o cansaço visual e melhorando a experiência geral de uso do sistema. As alterações incluem a mudança da paleta de cores para tons mais escuros, ajustes na tipografia para melhor legibilidade e a adição de elementos visuais que complementam o novo tema.

--- app.py
+++ app.py
@@ -13,11 +13,15 @@ app.register_blueprint(usuario_bp)
 # Rota do Dashboard (Página Inicial após login)
 @app.route('/dashboard')
 def dashboard():
+     # 1. Instancia o repositório que acessa o MySQL
     repo = RepositorioUsuarios()
+    # 2. Chama o método que traz todos os usuários do banco para uma lista
     usuarios = repo.listar()
+    # 3. Usa a função len() do Python para contar quantos itens existem nessa lista
     total_u = len(usuarios)
     
     # Dados fictícios para os chamados (serão substituídos pela API no futuro)
+    # 4. Envia esse número para o arquivo HTML através da variável 'total_usuarios'
     return render_template('dashboard.html', 
                            total_usuarios=total_u,
                            total_chamados_abertos=15,
